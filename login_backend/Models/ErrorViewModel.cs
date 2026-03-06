8c7e55cc16550710d962a0f79932fe9843c67801
Ridaa Mohamed <mridaa001@gmail.com>
2026-03-06 12:46:32 +0000

    add ASP.NET Core MVC backend project

---
+++ login_backend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace login_backend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
