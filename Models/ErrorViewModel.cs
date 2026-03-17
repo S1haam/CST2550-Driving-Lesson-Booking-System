7c0736caaec70dd94b0b4fc56839d470a811cec7
Eloise <158495694+Eloise25@users.noreply.github.com>
2026-03-17 13:46:18 +0000

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DrivingLessonBookingSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
