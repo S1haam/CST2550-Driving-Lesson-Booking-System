84710bd8e82ca2a93174fad7762cd357bd6ce6be
Eloise <158495694+Eloise25@users.noreply.github.com>
2026-03-18 13:29:13 +0000

    Add files via upload

---
+++ Models/Learner.cs
@@ -0,0 +1,22 @@
+﻿namespace DrivingLessonBookingSystem.Models
+{
+    public class Learner
+    {
+        public int LearnerID { get; set; }
+        public string LearnerName { get; set; }
+        public string LearnerLicenceID { get; set; }
+        public string LearnerEmail { get; set; }
+        public string LearnerPhone { get; set; }
+
+        public string LearnerPasswordHash { get; set; }
+        public string LearnerStatus { get; set; }
+
+        //Custom Lists
+        public CustomLessonList LearnerPastLessons { get; set; } = new CustomLessonList();
+        public CustomLessonList LearningUpcomingLessons { get; set; } = new CustomLessonList();
+
+        public int PastLessonCount { get; set; }
+        public int NextLessonCount { get; set; }
+
+    }
+}
