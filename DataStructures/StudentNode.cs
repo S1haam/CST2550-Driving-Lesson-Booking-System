96b1301e7c62daa144e0c58be9a236f47834450c
Eloise <158495694+Eloise25@users.noreply.github.com>
2026-03-18 12:11:31 +0000

    Add files via upload

---
+++ DataStructures/StudentNode.cs
@@ -0,0 +1,16 @@
+﻿using DrivingLessonBookingSystem.Models;
+
+namespace DrivingLessonBookingSystem.DataStructures
+{
+    public class StudentNode
+    {
+        public Student Data { get; set; }
+        public StudentNode Next { get; set; } // Points to the next StudentNode
+
+        public StudentNode(Student data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+}
---
+++ Models/Instructor.cs
@@ -0,0 +1,11 @@
+﻿namespace DrivingLessonBookingSystem.Models
+{
+    public class Instructor
+    {
+        public int IntructorID { get; set; }
+        public string IntructorName { get; set; }
+        public string IntructorEmail { get; set; }
+        public string IntructorPhone { get; set; }
+
+    }
+}
