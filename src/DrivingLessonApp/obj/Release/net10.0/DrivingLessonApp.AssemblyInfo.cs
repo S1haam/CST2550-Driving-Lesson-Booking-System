bad06d9de01d87bd31fc5afd63d11d89d0d62195
sihaam muuse ibraahim <muuseibraahimsihaam@gmail.com>
2026-03-30 18:18:25 -0700

    added login page to blazor

--- src/DrivingLessonApp/obj/Release/net10.0/DrivingLessonApp.AssemblyInfo.cs
+++ src/DrivingLessonApp/obj/Release/net10.0/DrivingLessonApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DrivingLessonApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+280a547e53486297301ad2dd5c559d95ae2fde00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9e1133bcd0699928af51618fca93e4fc88a1619")]
 [assembly: System.Reflection.AssemblyProductAttribute("DrivingLessonApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DrivingLessonApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
