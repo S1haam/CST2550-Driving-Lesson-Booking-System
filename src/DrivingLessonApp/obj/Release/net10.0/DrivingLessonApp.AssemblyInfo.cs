b93da59946120349007ad4a3354c7f548628a691
sihaam muuse ibraahim <muuseibraahimsihaam@gmail.com>
2026-03-30 19:04:52 -0700

    implemented lesson type and instructor selection flow

--- src/DrivingLessonApp/obj/Release/net10.0/DrivingLessonApp.AssemblyInfo.cs
+++ src/DrivingLessonApp/obj/Release/net10.0/DrivingLessonApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DrivingLessonApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9e1133bcd0699928af51618fca93e4fc88a1619")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bad06d9de01d87bd31fc5afd63d11d89d0d62195")]
 [assembly: System.Reflection.AssemblyProductAttribute("DrivingLessonApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DrivingLessonApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
