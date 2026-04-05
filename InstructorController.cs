7acc01279cc3e8de83d72535e467486430abd84a
r-ali0 <rayanssali05@gmail.com>
2026-04-05 01:29:49 +0100

    Instructor Routes and Notification Model
    All api routes for the instructor for inbox, searching students, accepting or declining a request, removing students, getting notified if lesson is rescheduled or cancelled and more
    
    Included Notification Model for Inbox
    
    The only thing that needs to be changed is the name for the project as this is named WebAPI. Also needs to be connected to the database as well.

---
+++ InstructorController.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class InstructorController
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
---
+++ Notification.cs
@@ -0,0 +1,29 @@
+﻿namespace WebAPI.Models
+{
+    public class Notification
+    {
+        //Notification specific Identifier
+        public int NotificationId { get; set; }
+
+        //Specific Instructor Identifier
+        public int InstructorId { get; set; }
+
+        //Message
+        public string Message { get; set; }
+
+        //Notification types = Rescheduled, Cancelled, Student Request and Removed student
+        public string NotificationType { get; set; }
+
+        //Time message was sent or recieved
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        //If message is read
+        public bool IsRead { get; set; } = false;
+
+        //If message is pinned
+        public bool IsPinned { get; set; } = false;
+
+        //If message is deleted
+        public bool IsDeleted { get; set; } = false;
+    }
+}
