505fbc15e6007d2040062dde4c301969bc05c26d
r-ali0 <rayanssali05@gmail.com>
2026-04-14 23:53:18 +0100

    Add Notifications DbSet to ApplicationDbContext
    I had made a new DbSet for Notifications for the Instructor's inbox

--- Backend-Connection/Data/ApplicationDbContext.cs
+++ Backend-Connection/Data/ApplicationDbContext.cs
@@ -19,6 +19,7 @@ namespace Backend_Connection.Data
         public DbSet<Admin> Admins { get; set; }
         public DbSet<StudentRequest> StudentRequests { get; set; }
         public DbSet<LessonOutcome> LessonOutcomes { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
 
         // Configure relationships + seed sample data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
